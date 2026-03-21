93faaae151e8b3be5b0d0cacec8f32e05f413dd9
Julian Aguilera <julianaguilera2005@gmail.com>
2026-03-21 00:25:47 +0000

    Mensaje describiendo los cambios

---
+++ Project/src/co/edu/unbosque/controller/Controller.java
@@ -0,0 +1,10 @@
+package co.edu.unbosque.controller;
+
+public class Controller {
+
+    public void run() {
+        // Lógica principal del programa
+        System.out.println("Ejecutando el controlador...");
+    }
+    
+}
